ea0924af895ec7ec75e47d5a0021234e661b6e14
肖恒 <xiaoheng03@meituan.com>
2020-08-02 16:17:03 +0800

    增加利用模型识别弹窗的功能

---
+++ vision-ML/all_config.py
@@ -0,0 +1,28 @@
+# -*- coding: UTF-8 -*-
+
+
+# image shape
+IMG_ROW, IMG_COL = 50, 50
+
+# model train
+batch_size = 30
+num_classes = 2
+epochs = 10
+
+# model name
+model_name = "model/trained_model_1.h5"
+
+# image path
+IMAGE_PATH = "image/"
+# IMAGE_PATH = "ios_update/good/"
+TRAIN_PATH = "train/"
+# TRAIN_PATH = "ios_update/"
+PREDICT_PATH = "predict/"
+
+# selective search
+SCALE = 2.0
+SIGMA = 0.8
+MIN_SIZE = 80
+
+# augmentation size for one class
+augmentation_size = 350
